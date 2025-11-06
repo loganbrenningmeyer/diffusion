9fe0fea78dbb7ebf791b216b82b2e1647a23fea8
Logan <loganbrenningmeyer@gmail.com>
2025-11-06 11:50:57 -0600

    added self-attention/decoder

--- src/diffusion/models/unet.py
+++ src/diffusion/models/unet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 
 
@@ -7,18 +8,16 @@ class ResBlock(nn.Module):
     def __init__(self, in_ch, out_ch, t_dim):
         super().__init__()
         # ---------
-        # Skip
+        # Skip Projection
         # ----------
         if in_ch != out_ch:
             self.skip = nn.Conv2d(in_ch, out_ch, kernel_size=1)
         else:
             self.skip = nn.Identity()
-        
         # ---------
         # Time Embedding Projection
         # ----------
         self.t_proj = nn.Linear(t_dim, out_ch)
-
         # ---------
         # Norms/Convolutions
         # ----------
@@ -45,7 +44,6 @@ class ResBlock(nn.Module):
 
         x += skip
         x *= (2 ** -0.5)
-        
         return x
 
 
@@ -66,7 +64,6 @@ class EncoderBlock(nn.Module):
         # ----------
         self.res1 = ResBlock(in_ch,  out_ch, t_dim)
         self.res2 = ResBlock(out_ch, out_ch, t_dim)
-
         # ---------
         # Downsampling
         # ----------
@@ -77,29 +74,94 @@ class EncoderBlock(nn.Module):
         x = self.res2(x, t_emb)
         skip = x
         x = self.down(x)
-
         return x, skip
 
 
-class AttentionBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-
 class Upsample(nn.Module):
-    def __init__(self, in_ch):
+    def __init__(self, in_ch, out_ch):
         super().__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1)
     
     def forward(self, x):
-        pass
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        return self.conv(x)
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, t_dim, up: bool):
+    def __init__(self, in_ch, out_ch, skip_ch, t_dim, up: bool):
         super().__init__()
-        
+        # ---------
+        # Upsampling
+        # ----------
+        self.up   = Upsample(in_ch, out_ch) if up else nn.Identity()
+        # ---------
+        # Residual Blocks
+        # ----------
+        res1_in_ch = out_ch + skip_ch if up else out_ch
+        self.res1  = ResBlock(res1_in_ch, out_ch, t_dim)
+        self.res2  = ResBlock(out_ch,     out_ch, t_dim)
 
+    def forward(self, x, skip, t_emb):
+        x = self.up(x)
+        x = torch.cat([x, skip], dim=1)
+        x = self.res1(x, t_emb)
+        x = self.res2(x, t_emb)
+        return x
+
+
+class SelfAttentionBlock(nn.Module):
+    def __init__(self, in_ch, num_heads):
+        super().__init__()
+        self.norm = nn.GroupNorm(32, in_ch)
+        self.attn = nn.MultiheadAttention(in_ch, num_heads, batch_first=True)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        # -- GroupNorm / Flatten
+        x_norm = self.norm(x)
+        x_flat = x_norm.flatten(2).permute(0,2,1)   # (B, H*W, C)
+        # -- Self-attention
+        attn_out, _ = self.attn(x_flat, x_flat, x_flat)
+        attn_out = attn_out.permute(0,2,1)          # (B, C, H*W)
+        attn_out = attn_out.view(b, c, h, w)        # (B, C, H, W)
+        return x + attn_out
 
 class UNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_ch=3, base_ch=128, ch_mults=[1,1,2,2,4,4]):
         super().__init__()
+        # ---------
+        # Time Embedding MLP
+        # ----------
+        t_dim = base_ch * 4
+        self.t_mlp = nn.Sequential(
+            nn.Linear(base_ch, t_dim),
+            nn.SiLU(),
+            nn.Linear(t_dim, t_dim)
+        )
+        # ---------
+        # Stem
+        # ----------
+        self.stem = nn.Sequential(
+            nn.Conv2d(in_ch, base_ch, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(32, base_ch),
+            nn.SiLU()
+        )
+        in_ch = base_ch
+        # ---------
+        # Encoder
+        # ----------
+
+        # ---------
+        # Bottleneck
+        # ----------
+
+        # ---------
+        # Decoder
+        # ----------
+
+        # ---------
+        # Final Layer
+        # ----------
+
+    def forward(self, x, t):
+        pass
